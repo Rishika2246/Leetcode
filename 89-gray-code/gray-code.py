b6aab8940bd4b06f75db93149df6d27552f7bccd
Rishika2246 <161246312+Rishika2246@users.noreply.github.com>
2026-06-25 23:01:02 +0530

    Time: 7 ms (72.88%) | Memory: 22.6 MB (50.22%) - LeetSync

---
+++ 89-gray-code/gray-code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = [0]
+        
+        for i in range(n):
+            for num in reversed(res):
+                res.append(num | (1 << i))
+        
+        return res
