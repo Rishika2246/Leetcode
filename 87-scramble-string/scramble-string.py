6351292589c217262cb95261a676eb1cea602974
Rishika2246 <161246312+Rishika2246@users.noreply.github.com>
2026-06-25 22:58:00 +0530

    Time: 15 ms (43.78%) | Memory: 21.4 MB (14.85%) - LeetSync

---
+++ 87-scramble-string/scramble-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        from functools import lru_cache
+        
+        @lru_cache(None)
+        def dfs(a, b):
+            if a == b:
+                return True
+            if sorted(a) != sorted(b):
+                return False
+            
+            n = len(a)
+            for i in range(1, n):
+                # no swap
+                if dfs(a[:i], b[:i]) and dfs(a[i:], b[i:]):
+                    return True
+                # swap
+                if dfs(a[:i], b[-i:]) and dfs(a[i:], b[:-i]):
+                    return True
+            
+            return False
+        
+        return dfs(s1, s2)
