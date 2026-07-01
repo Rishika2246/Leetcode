0f3c31e356107b46f826f59af922ad655236a15d
Rishika Reddy <161246312+Rishika2246@users.noreply.github.com>
2026-07-01 14:13:01 +0530

    Time: 3 ms (84.10%) | Memory: 22.5 MB (27.65%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board or not board[0]:
+            return
+        
+        m, n = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or board[r][c] != 'O':
+                return
+            
+            board[r][c] = 'T'
+            
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        # Mark border-connected 'O's
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+        
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+        
+        # Flip and restore
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
