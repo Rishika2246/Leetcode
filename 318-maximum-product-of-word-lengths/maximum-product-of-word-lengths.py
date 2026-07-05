43d3cd823a02b9ce69e981cf85efab8ebd1829d4
Rishika Reddy <161246312+Rishika2246@users.noreply.github.com>
2026-07-05 20:50:14 +0530

    Time: 221 ms (62.67%) | Memory: 22.3 MB (45.86%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        masks = []
+
+        for word in words:
+            mask = 0
+            for ch in word:
+                mask |= 1 << (ord(ch) - ord('a'))
+            masks.append(mask)
+
+        ans = 0
+
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                # No common letters
+                if masks[i] & masks[j] == 0:
+                    ans = max(ans, len(words[i]) * len(words[j]))
+
+        return ans
