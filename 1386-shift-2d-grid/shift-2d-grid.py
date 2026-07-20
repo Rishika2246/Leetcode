dd4a282a8c8b7adeee63b871eea8bc4ac2440446
Rishika Reddy <161246312+Rishika2246@users.noreply.github.com>
2026-07-20 18:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (19.55%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        arr = [x for row in grid for x in row]
+        k %= m * n
+        arr = arr[-k:] + arr[:-k]
+        return [arr[i * n:(i + 1) * n] for i in range(m)]
