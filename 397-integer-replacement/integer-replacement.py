557ccb7bf601cc8aa0e592b51f97db316c930f1c
Rishika Reddy Sanikommu <161246312+Rishika2246@users.noreply.github.com>
2026-07-24 13:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.16%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        ans = 0
+        while n != 1:
+            if n % 2 == 0:
+                n //= 2
+            elif n == 3 or (n & 3) == 1:
+                n -= 1
+            else:
+                n += 1
+            ans += 1
+        return ans
